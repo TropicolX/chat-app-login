41f5b7b24f9845d3669a74927ccb2b843b2cfc5e
TropicolCoder <jacobsbusayo@gmail.com>
2021-07-02 20:23:50 +0100

    Responsive fix

--- js/app.js
+++ js/app.js
@@ -4,8 +4,11 @@ const container = document.querySelector(".container");
 
 get_started_btn.addEventListener("click", () => {
 	container.classList.add("join-mode");
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
 
 back_icon.addEventListener("click", () => {
 	container.classList.remove("join-mode");
 });
+
